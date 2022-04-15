4deb0b4b5feb9405bf014e22e0cb7b9581e45c12
Ashutosh-png <98956581+Ashutosh-png@users.noreply.github.com>
2022-04-16 02:24:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+
+
+function compute() {
+    let p = document.getElementById("principal").value;
+    let r = document.getElementById("rate").value;
+    let y = document.getElementById("years").value;
+    let ans= document.getElementById("show");
+      let rate= r/100
+    ans.innerHTML= "if you deposite "+p+
+                 " at an interest rate of  "+r+"%"+
+                 " you will receive "+p*rate*y+" in "+y+" years"
+    
+    
+    
     
 }
         
